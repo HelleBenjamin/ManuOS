1880cddafbd7f580dc8808eb5c7473e369477d61
Pepe57 <bensku2008@gmail.com>
2024-07-10 18:37:36 +0300

    update

--- ManuOS-asm/puppy_kernel.asm
+++ ManuOS-asm/puppy_kernel.asm
@@ -1,7 +1,8 @@
-bits 16
+BITS 16
+
 section .data
     welcome db 'Welcome to ManuOS 0.0.1-alpha', 0
-section .bss
+
 section .text
     global start
     global halt
@@ -10,6 +11,8 @@ section .text
     global print_chr
     global print_str
     global clrscr
+    global save_byte_to_disk
+    global read_byte_from_disk
 
     extern main
 
@@ -60,4 +63,81 @@ clrscr: ; Parameters: none Returns: none
     mov ah, 0x00
     mov al, 0x03
     int 0x10
+    ret
+
+save_byte_to_disk: ; Parameters: al = byte, bx = address on disk Returns: none
+    pusha                   ; Save all general-purpose registers
+    push es                 ; Save ES register
+
+    ; Write one sector (512 bytes) from memory address 0x0600
+    mov ax, 0x0600
+    mov es, ax
+    mov di, bx              ; Destination address in ES:DI
+    stosb                   ; Store byte in AL to [ES:DI]
+
+    ; BIOS disk write parameters
+    mov bx, 0x0600          ; Data buffer address in memory
+    mov ah, 0x03            ; Write sector function
+    mov al, 1               ; Number of sectors to write
+    mov ch, 0               ; Cylinder number
+    mov cl, 2               ; Sector number (2 since 1 is typically used by the boot sector)
+    mov dh, 0               ; Head number
+    mov dl, 0               ; Drive number (0 for the first floppy drive)
+    int 0x13                ; Call BIOS interrupt
+
+    jc write_error          ; Jump if carry flag is set (indicating an error)
+    jmp write_success
+
+write_error:
+    ; Handle the error
+    call print_str
+    db 'Disk write error', 0
+    jmp halt
+
+write_success:
+    ; Successfully written to disk
+    call print_str
+    db 'Disk write successful', 0
+
+    pop es                  ; Restore ES register
+    popa                    ; Restore all general-purpose registers
+    ret
+
+read_byte_from_disk: ; Parameters: bx = address on disk Returns: al = byte
+    pusha                   ; Save all general-purpose registers
+    push es                 ; Save ES register
+
+    ; BIOS disk read parameters
+    mov bx, 0x0600          ; Data buffer address in memory
+    mov ah, 0x02            ; Read sector function
+    mov al, 1               ; Number of sectors to read
+    mov ch, 0               ; Cylinder number
+    mov cl, 2               ; Sector number (2 since 1 is typically used by the boot sector)
+    mov dh, 0               ; Head number
+    mov dl, 0               ; Drive number (0 for the first floppy drive)
+    int 0x13                ; Call BIOS interrupt
+
+    jc read_error           ; Jump if carry flag is set (indicating an error)
+
+    ; Load the byte from memory address 0x0600 + offset
+    mov ax, 0x0600
+    mov es, ax
+    mov di, bx              ; Source address in ES:DI
+    lodsb                   ; Load byte from [ES:DI] to AL
+
+    jmp read_success
+
+read_error:
+    ; Handle the error
+    call print_str
+    db 'Disk read error', 0
+    jmp halt
+
+read_success:
+    ; Successfully read from disk
+    call print_str
+    db 'Disk read successful', 0
+
+    pop es                  ; Restore ES register
+    popa                    ; Restore all general-purpose registers
     ret
