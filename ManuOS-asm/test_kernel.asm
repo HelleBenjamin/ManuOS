dee46be2b564ae8145ae6eb7150ab0e5db0dd1c2
Pepe57 <bensku2008@gmail.com>
2024-07-10 23:08:36 +0300

    update

--- ManuOS-asm/test_kernel.asm
+++ ManuOS-asm/test_kernel.asm
@@ -1,26 +1,81 @@
-bits 16
+BITS 16
 
 section .data
+    welcome db 'Welcome to ManuOS 0.0.1-alpha', 0
     version db 'ManuOS 0.0.1-puppy, Puppy-kernel 0.0.1', 0
-    help db 'Commands: m(manu), v(version), h(help), t(text editor)', 0
+    help db 'Commands: m(manu), v(version), h(help), t(text editor), w(wuf++ interpreter)', 0
     not_found db 'Command not found: ', 0
     txt_msg db 'Text editor v1.2 ', 0
     escmsg db 'Press ESC to exit', 0
     manumsg db 'Manu', 0
     wppmsg db 'Welcome to Wuf++ interpreter, press enter to run the code', 0
+    wpp_help_msg db 'Commands: w(write), r(read), q(quit)', 0
 
 section .bss
-    cmd_buffer resb 0x20
+    cmd_buffer resb 0x50
     wpp_buffer resb 0x100
 
 section .text
-    global main
-    extern read
-    extern print_chr
-    extern print_str
-    extern newline
-    extern clrscr
-    extern halt
+    global start
+    global halt
+    global newline
+    global read
+    global print_chr
+    global print_str
+    global clrscr
+    global save_byte_to_disk
+    global read_byte_from_disk
+
+    extern main
+
+start:
+    call newline
+    mov bx, welcome
+    call print_str
+    call newline
+    jmp main
+
+; System calls
+
+halt: ; Parameters: none Returns: none
+    nop
+    jmp halt
+
+newline: ; Parameters: none Returns: none
+    mov al, 0x0a
+    call print_chr
+    mov al, 0x0d
+    call print_chr
+    ret
+
+read: ; Parameters: none Returns: al = character
+    mov ah, 0x00
+    int 0x16
+    ret
+
+print_chr: ; Parameters: al = character Returns: none
+    mov ah, 0x0e
+    int 0x10
+    ret
+
+print_str: ; Parameters: bx = pointer to string Returns: none
+    mov ah, 0x0e
+    .print_str_loop:
+        mov al, [bx]
+        cmp al, 0
+        je .end_print_str
+        int 0x10
+        inc bx
+        jmp .print_str_loop
+
+    .end_print_str:
+        ret
+
+clrscr: ; Parameters: none Returns: none
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
+    ret
 
 main:
     jmp terminal
@@ -115,6 +170,7 @@ check_txt_functions:
         call print_chr
         .no_backspace:
             ret
+
 ; Wuf++ - Brainfuck like code interpreter
 ; Registers that are used:
 ; al - main register
