e2af3c57cc8b3382d58bc60c652aa9960b033caa
Pepe57 <bensku2008@gmail.com>
2024-07-11 22:52:54 +0300

    update

--- ManuOS-C/kernel.c
+++ ManuOS-C/kernel.c
@@ -6,6 +6,7 @@
 extern void getchar();
 extern void nl();
 extern void cls();
+extern void wpp_interpreter();
 
 
 void ascii_to_hex(char *str) {
@@ -54,91 +55,7 @@ short m_strcmp(char *str1, char *str2) {
     }
     return *(unsigned char *)str1 - *(unsigned char *)str2;
 }
-/* Wuf++ - Brainfuck like code interpreter
-; Registers that are used:
-; bl - main register
-; cx - pointer, not the program counter
-; + - increment main register
-; - - decrement main register
-; > - push main register
-; < - pop main register
-; . - print main register
-; , - read to the main register
-; [ - jump forward, by cx
-; ] - jump back, by cx
-; ! - invert main register
-; } - increment pointer
-; { - decrement pointer
-; ? - print pointer
-*/
 
-void wpp_interpreter(void) {
-    start:
-    cls();
-    char *wpp_buffer;
-    char bl, cx = 0;
-    unsigned int i = 0;
-    prints("Welcome to Wuf++ interpreter, press enter to run the code");
-    nl();
-    while (1) {
-        i = 0;
-        while (1) {
-            wpp_buffer[i] = getc();
-            if (wpp_buffer[i] == NEWLINE) {
-                wpp_buffer[i] = '\0';
-                break;
-            }
-            printc(wpp_buffer[i]);
-            i++;
-        }
-        for (i = 0; wpp_buffer[i] != '\0'; i++) {
-            switch (wpp_buffer[i])
-            {
-            case '+':
-                bl++;
-                break;
-            case '-':
-                bl--;
-                break;
-            case '>':
-                asm(
-                    "nop"
-                    : 
-                    : "bl"(bl)
-                );
-                asm("push %bx");
-                break;
-            case '<':
-                asm("pop %bx");
-                asm(
-                    "nop"
-                    : "=bl"(bl)
-                    :
-                );
-                break;
-            case '.':
-                printc(bl);
-                break;
-            case ',':
-                bl = getch();
-                break;
-            case '[':
-                i = i - cx;
-                break;
-            case ']':
-                i = i + cx;
-                break;
-            default:
-                break;
-            }
-        }
-        nl();
-        prints("Code finished, press enter to run the code again");
-        getch();
-        break;
-    }
-    goto start;
-}
 /* Wuf+ - Easier version of Wuf++
 
 Registers that are used:
--- ManuOS-C/kernel_asm.asm
+++ ManuOS-C/kernel_asm.asm
@@ -1,6 +1,12 @@
 
 bits 16
 
+section .data
+    wppmsg db 'Welcome to Wuf++ interpreter, press enter to run the code', 0
+    fmsg db 'Program finished, press enter to proceed', 0
+section .bss
+    wpp_buffer resb 0x100
+section .text
 global _start
 extern main
 
@@ -38,3 +44,196 @@ cls:
     mov al, 0x03
     int 0x10
     ret
+
+global printstr
+print_str: ; Parameters: bx = pointer to string Returns: none
+    mov ah, 0x0e
+    .print_str_loop:
+        mov al, [bx]
+        cmp al, 0
+        je .end_print_str
+        int 0x10
+        inc bx
+        jmp .print_str_loop
+
+    .end_print_str:
+        ret
+
+; Wuf++ - Brainfuck like code interpreter
+; Registers that are used:
+; bl - main register
+; cx - pointer, not the program counter
+; Example: #H.#e.#l.#l.#o.
+;Syntax:
+; + - increment main register
+; - - decrement main register
+; > - push main register
+; < - pop main register
+; . - print main register
+; , - read to the main register
+; & - jump to location pointed by pointer
+; [ - pc = pc - cx
+; ] - pc = pc + cx
+; ! - invert main register
+; } - increment pointer
+; { - decrement pointer
+; $ - print pointer
+; #<char> - load char to main register
+; ( - loop start, decrement pointer and loop until pointer = 0
+; ) - loop end
+; " - swap registers
+; %<char> - compare main register with char, jump if equal to location pointed by pointer
+; = - halt
+global wpp_interpreter
+wpp_interpreter:
+    .init_interpreter:
+        call cls
+        mov bx, wppmsg
+        call print_str
+        call nl
+        mov di, wpp_buffer
+        mov cx, 0x0000 ; Pointer
+        mov bx, 0x0000 ; Main register
+    .interpreter_loop:
+        call getchar
+        mov [di], al
+        inc di
+        call printchr
+        cmp al, 0x0d
+        je .handle_enter
+        cmp al, 0x08
+        je .handle_backspace
+        jne .interpreter_loop
+    .handle_backspace:
+        dec di
+        dec di
+        mov byte [di], 0
+        jmp .interpreter_loop
+    .handle_enter:
+        mov byte [di], 0
+        mov di, wpp_buffer
+        call nl
+    .interpret:
+        mov al, [di]
+        cmp di, 0
+        je .end_interpreter
+        inc di
+        cmp al, '+'
+        je .if_plus
+        cmp al, '-'
+        je .if_minus
+        cmp al, '>'
+        je .if_push
+        cmp al, '<'
+        je .if_pop
+        cmp al, '.'
+        je .if_print
+        cmp al, ','
+        je .if_read
+        cmp al, '&'
+        je .if_jump
+        cmp al, '['
+        je .if_jump_back
+        cmp al, ']'
+        je .if_jump_forward
+        cmp al, '!'
+        je .if_invert
+        cmp al, '}'
+        je .if_increment_pointer
+        cmp al, '{'
+        je .if_decrement_pointer
+        cmp al, '$'
+        je .if_print_pointer
+        cmp al, '#'
+        je .if_load_char
+        cmp al, '('
+        je .if_loop_start
+        cmp al, ')'
+        je .if_loop_end
+        cmp al, '"'
+        je .if_swap
+        cmp al, '%'
+        je .if_compare
+        cmp al, '='
+        je .if_halt
+        jne .interpret
+        .if_read:
+            .read_loop:
+                call getchar
+                cmp al, 0
+                je .read_loop
+            mov bl, al
+            jmp .interpret
+        .if_plus:
+            inc bl
+            jmp .interpret
+        .if_minus:
+            dec bl
+            jmp .interpret
+        .if_push:
+            push bx
+            jmp .interpret
+        .if_pop:
+            pop bx
+            jmp .interpret
+        .if_print:
+            mov al, bl
+            call printchr
+            jmp .interpret
+        .if_jump:
+            mov di, cx
+            jmp .interpret
+        .if_jump_back:
+            sub di, cx
+            jmp .interpret
+        .if_jump_forward:
+            add di, cx
+            jmp .interpret
+        .if_invert:
+            not bl
+            jmp .interpret
+        .if_increment_pointer:
+            inc cx
+            jmp .interpret
+        .if_decrement_pointer:
+            dec cx
+            jmp .interpret
+        .if_print_pointer:
+            mov al, cl
+            call printchr
+            jmp .interpret
+        .if_load_char:
+            mov bl, [di]
+            inc di
+            jmp .interpret
+        .if_loop_start:
+            mov dx, di
+            jmp .interpret
+        .if_loop_end:
+            cmp cx, 0
+            je .interpret
+            dec cx
+            mov di, dx
+            jmp .interpret
+        .if_swap:
+            xchg bl, cl
+            jmp .interpret
+        .if_compare:
+            mov ah, [di]
+            inc di
+            cmp bl, ah
+            jne .interpret
+            mov di, cx
+            jmp .interpret
+        .if_halt:
+            jmp .end_interpreter
+    .end_interpreter:
+        call nl
+        mov bx, fmsg
+        call print_str
+        .l1:
+            call getchar
+            cmp al, 0x0d
+            jne .l1
+        call cls
+        ret
